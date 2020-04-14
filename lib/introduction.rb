4609573dca30013d90ad67f0c52d6bf01b89060e
Eulis Bonifacio <eulis01@gmail.com>
2020-04-14 21:12:45 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,4 +6,4 @@ introduction("Eulis")
 def introduction_with_language(a, b)
   puts "Hi, my name is #{a} and I am learning to program in #{b}."
 end
-introduction_with_language("Eulis", "Ruby".)
+introduction_with_language("Eulis", "Ruby")
